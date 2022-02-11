10d690c35e44d7ec51c8eab2018cee0657251e75
Jason Dellaluce <jasondellaluce@gmail.com>
2022-02-11 15:04:32 +0000

    update(sdk): support list field types according to plugin API version 1.0.0
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- pkg/sdk/sdk.go
+++ pkg/sdk/sdk.go
@@ -107,11 +107,12 @@ const (
 // FieldEntry represents a single field entry that an extractor plugin can expose.
 // Should be used when implementing plugin_get_fields().
 type FieldEntry struct {
-	Type        string `json:"type"`
-	Name        string `json:"name"`
-	ArgRequired bool   `json:"argRequired"`
-	Display     string `json:"display"`
-	Desc        string `json:"desc"`
+	Name        string   `json:"name"`
+	Type        string   `json:"type"`
+	IsList      bool     `json:"isList"`
+	ArgRequired bool     `json:"argRequired"`
+	Display     string   `json:"display"`
+	Desc        string   `json:"desc"`
 	Properties  []string `json:"properties"`
 }
 
